f4210c7969f0c5e676f519851fa236f8a8b95b5b
Daniel Melonari <daniel_melonari@hotmail.com>
2023-06-22 20:14:06 -0300

    Alterado cores do aplicativo conforme opiniao da Carla (minha esposa)

--- lib/features/core/ui/cores.dart
+++ lib/features/core/ui/cores.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 class Cores {
   static const primary = Color(0XFFA28EE8);
-  static const scaffold = Color(0XFFD8B8F2);
+  static const scaffold = Color(0XFFE8E8E8);
   static const appBar = Color(0XFF6C4D91);
-  static const fab = Color(0XFFF7D651);
+  static const fab = Color(0XFFE88EAB);
   static const title = Color(0XFF444444);
   static const text = Color(0XFF888888);
 }
